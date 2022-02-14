cf7003d26c4b0817523f67d078df22e7eb023e9f
ivanbenko <ivan@krafteq.de>
2022-02-14 16:10:55 +0100

    minor fixes

--- packages/fungible-token/src/streams.ts
+++ packages/fungible-token/src/streams.ts
@@ -1,13 +1,16 @@
 import * as events from "./events";
 import { EventStreamSchema, serializers } from "@proxima-one/stream-schema-base";
+import { BlockchainReference } from "./model";
 
 type FungibleTokenStreamEvent =
   | events.NewToken
   | events.Approval
-  | events.Transfer;
+  | events.Transfer & {
+  ref?: BlockchainReference
+};
 
 export const fungibleToken: EventStreamSchema<FungibleTokenStreamEvent> = {
-  name: "ft.streams.proxima.one",
+  name: "fungible-token.streams.proxima.one",
   serdes: serializers.json<FungibleTokenStreamEvent>(),
   version: "0.1",
 }
