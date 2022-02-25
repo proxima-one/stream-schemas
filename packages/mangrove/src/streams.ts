c08682d1c49e649cb5e55fb901f962be83749cd0
ivanbenko <ivan@krafteq.de>
2022-02-25 21:19:04 +0100

    mangrove domain event stream

---
+++ packages/mangrove/src/streams.ts
@@ -0,0 +1,15 @@
+import * as eth from "./eth";
+import * as core from "./core";
+import { MangroveEvent } from "./events";
+import { EventStreamSchema, serializers } from "@proxima-one/stream-schema-base";
+
+export type MangroveStreamEvent = MangroveEvent & {
+  tx: eth.TransactionRef;
+  mangroveId: core.MangroveId; //support multiple instances in the same event stream
+};
+
+export const mangrove: EventStreamSchema<MangroveStreamEvent> = {
+  name: "mangrove.streams.proxima.one",
+  serdes: serializers.json<MangroveStreamEvent>(),
+  version: "0.1.0",
+}
