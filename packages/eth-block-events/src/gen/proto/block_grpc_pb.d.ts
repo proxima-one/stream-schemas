c20bf316ca302555cd6e1946fcf41c9493813c02
Chase Smith <chasesmith@berkeley.edu>
2022-04-28 07:16:16 -0700

    Adding package information

--- packages/eth-block-events/src/gen/proto/block_grpc_pb.d.ts
+++ packages/eth-block-events/src/gen/proto/block_grpc_pb.d.ts
@@ -1,4 +1,4 @@
-// package: 
+// package:
 // file: proto/block.proto
 
 /* tslint:disable */
@@ -8,18 +8,23 @@ import * as grpc from "@grpc/grpc-js";
 import * as proto_block_pb from "../proto/block_pb";
 import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
 
-interface IEthereumTransactionsService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
-}
-
+interface IEthereumTransactionsService
+  extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {}
 
 export const EthereumTransactionsService: IEthereumTransactionsService;
 
-export interface IEthereumTransactionsServer extends grpc.UntypedServiceImplementation {
-}
+export interface IEthereumTransactionsServer
+  extends grpc.UntypedServiceImplementation {}
 
-export interface IEthereumTransactionsClient {
-}
+export interface IEthereumTransactionsClient {}
 
-export class EthereumTransactionsClient extends grpc.Client implements IEthereumTransactionsClient {
-    constructor(address: string, credentials: grpc.ChannelCredentials, options?: Partial<grpc.ClientOptions>);
+export class EthereumTransactionsClient
+  extends grpc.Client
+  implements IEthereumTransactionsClient
+{
+  constructor(
+    address: string,
+    credentials: grpc.ChannelCredentials,
+    options?: Partial<grpc.ClientOptions>
+  );
 }
--- packages/eth-block-events/src/streams.ts
+++ packages/eth-block-events/src/streams.ts
@@ -1,32 +1,39 @@
-import {Block, BlockHeader, Transaction, Log, Receipt} from '../src/gen/ts/proto/block';
-import { EventStreamSchema, serializers } from "@proxima-one/stream-schema-base";
+import {
+  Block,
+  BlockHeader,
+  Transaction,
+  Log,
+  Receipt,
+} from "../src/gen/ts/proto/block";
+import {
+  EventStreamSchema,
+  serializers,
+} from "@proxima-one/stream-schema-base";
 
-
-export type EthBlockEvent = Block | BlockHeader | Log | 
-Receipt | Transaction;
+export type EthBlockEvent = Block | BlockHeader | Log | Receipt | Transaction;
 
 export class BlockProtoSerializer {
   serialize(val: Block): Buffer {
-    return Buffer.from(Block.encode(val).finish())
+    return Buffer.from(Block.encode(val).finish());
   }
 
   deserialize(buf: Buffer): Block {
-    return Block.decode(buf)
+    return Block.decode(buf);
   }
 }
 
 export class BlockHeaderProtoSerializer {
   serialize(val: BlockHeader): Buffer {
-    return Buffer.from(BlockHeader.encode(val).finish())
+    return Buffer.from(BlockHeader.encode(val).finish());
   }
 
   deserialize(buf: Buffer): BlockHeader {
-    return BlockHeader.decode(buf)
+    return BlockHeader.decode(buf);
   }
 }
 
 export const fungibleToken: EventStreamSchema<EthBlockEvent> = {
-    name: "eth-block-events.streams.proxima.one",
-    serdes: new BlockProtoSerializer(),
-    version: "0.1.0",
-  }
+  name: "eth-block-events.streams.proxima.one",
+  serdes: new BlockProtoSerializer(),
+  version: "0.1.0",
+};
